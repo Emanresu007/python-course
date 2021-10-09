cc0cbd7be81ac3089cbcc42e10c1ffe5c17ebd13
Aliev Fetibek <feti.aliev@gmail.com>
2021-10-09 21:11:32 +0300

    While True

--- PyCharm 5.py
+++ PyCharm 5.py
@@ -1,6 +1,15 @@
-x = [1, 2, 4, 1 ,6 ]
-a.reverse()
+def greet(name):
+    if name[0].isupper():
+        return "Hello, " + name
+    else:
+        raise ValueError(name + " is inappropriate name")
 
-k = x.index(1)
-
-print(k)
+while True:
+    try:
+        name = input("Please enter your name, my Dear friend: ")
+        greeting = greet(name)
+        print(greeting)
+    except ValueError:
+        print("incorrect, Please try again your name with title first letter")
+    else:
+        break
